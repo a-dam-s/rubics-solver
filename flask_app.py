cca78bd529986b9f15a848ecf9789fe235f142d8
Adam S <svobodaadam007@gmail.com>
2023-09-15 22:59:43 +0200

    WORKING

--- flask_app.py
+++ flask_app.py
@@ -18,9 +18,11 @@ def solve(scramble):
     for i in moves:
         if i in list("rludfb"):
             moves_copy.append(i)
+        elif i == "2":
+            moves_copy.append(moves_copy[-1])
         else:
-            moves_copy[-1] += i
+            moves_copy[-1] += i 
     for i in moves_copy:
         images += "<img src='/static/" + i + ".png'>"
-    response_string = "<style>img{width:100px;height:100px;}</style><h3>Before applying the solution, apply those steps:</h3><br><img src='/static/x.png'><img src='/static/x.png'><img src='/static/y.png'> <br> solution: " + solution[0]  +  "<br>" + images + "<br> solve time: " + str(solution[1])
+    response_string = "<style>img{width:100px;height:100px;}</style><h3>Before applying the solution, apply those steps:</h3><br><img src='/static/x.png'><img src='/static/x.png'><img src='/static/y.png'> <br> solution: " + solution[0]  +  "<br>" + images + "<br> solve time: " + str(solution[1]) + "<br><a href='http://truet0123.eu.pythonanywhere.com'>Try again</a>"
     return Response(status=200, response=response_string)
