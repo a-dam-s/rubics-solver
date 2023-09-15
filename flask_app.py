87cbc3d6fc7d5700c24a9367fc69f1351bd827ba
Adam S <svobodaadam007@gmail.com>
2023-09-15 22:38:40 +0200

    prime names

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ def solve(scramble):
     solution = start_solve(scramble)
     if solution == -1:
         return Response(status=400, response='Invalid scramble')
-    moves = list(solution[0].lower().replace(" ", ""))
+    moves = list(solution[0].lower().replace(" ", "").replace("'", "p"))
     images = ""
     moves_copy = []
     for i in moves:
@@ -22,5 +22,5 @@ def solve(scramble):
             moves_copy[-1] += i
     for i in moves_copy:
         images += "<img src='/static/" + i + ".png'>"
-    response_string = "<style>img{width:100px;height:100px;}</style><h3>Before applying the solution, apply those steps:</h3><br><img src='/static/x.png'><img src='/static/x.png'><img src='/static/y.png'> <br> solution: " + solution[0] + "<br> solve time: " + str(solution[1]) +  "<br>" + images
+    response_string = "<style>img{width:100px;height:100px;}</style><h3>Before applying the solution, apply those steps:</h3><br><img src='/static/x.png'><img src='/static/x.png'><img src='/static/y.png'> <br> solution: " + solution[0]  +  "<br>" + images + "<br> solve time: " + str(solution[1])
     return Response(status=200, response=response_string)
